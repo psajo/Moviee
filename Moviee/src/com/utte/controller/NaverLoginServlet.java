ca42cc746ac77e094ca997996648b270d3a7767c
psajo <psajo@DESKTOP-AGJQBF5>
2020-11-27 22:18:06 +0900

    paging

--- Moviee/src/com/utte/controller/NaverLoginServlet.java
+++ Moviee/src/com/utte/controller/NaverLoginServlet.java
@@ -10,8 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class NaverLogin
  */
-@WebServlet("/NaverLogin")
-public class NaverLogin extends HttpServlet {
+@WebServlet("/NaverLoginServlet")
+public class NaverLoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	
--- Moviee/src/com/utte/dao/ReviewDAO.java
+++ Moviee/src/com/utte/dao/ReviewDAO.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.utte.beans.Member;
+import com.utte.beans.Paging;
 import com.utte.beans.Review;
 
 public class ReviewDAO<Int> {
@@ -45,8 +46,87 @@ public class ReviewDAO<Int> {
 		}
 		return ret;
 	}
-	
-	//******* 리뷰 목록 ******//
+	//해당 영화의 총 리뷰수
+	public static int getReviewCountByMvId(String mv_id) {
+		int count=-1;//리뷰 수를 저장
+		Connection conn = null; 
+		PreparedStatement ps = null; 
+		ResultSet rs = null;  
+		try { 
+			conn = MyConnection.getConnection(); 
+			String sql="SELECT COUNT(*) FROM review WHERE mv_id=?";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1,mv_id);
+			rs =ps.executeQuery();
+			if(rs.next()) {
+				count =rs.getInt("COUNT(*)");
+				System.out.println("count : "+count);
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace(); // e로 오류 받아서 오류 어디서 발생했는지 콘솔에 띄우기
+		}finally {
+			try {
+				if(conn!= null) 
+					conn.close();
+				if(ps != null) 
+					ps.close();
+				if(rs != null)
+					rs.close();
+			}catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+		return count;
+	}
+	//해당 영화의 최신 리뷰순으로 페이징된 결과
+	public static List<Review> getReviewsByMvId(String mv_id,int page) {
+		int count = getReviewCountByMvId(mv_id);
+		Paging paging = new Paging(count);
+		paging.setPaging(page);
+		Connection conn = null; 
+		PreparedStatement ps = null; 
+		ResultSet rs = null;
+		List<Review> list = null;
+		try { 
+			conn = MyConnection.getConnection(); 
+			String sql="SELECT * FROM (SELECT * FROM review WHERE mv_id=?) WHERE rownum >=? and rownum<= ?";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1,mv_id);
+			ps.setInt(2, paging.getStartRownum());
+			ps.setInt(3, paging.getEndRownum());
+			rs =ps.executeQuery();
+			if(rs.next()) {
+				list = new ArrayList<>();
+				do {
+					Review rvo = new Review();
+					rvo.setR_reviewId(rs.getInt("r_reviewId"));
+					rvo.setM_nick(rs.getString("m_nick"));
+					rvo.setMv_id(rs.getInt("mv_id"));
+					rvo.setR_contents(rs.getString("r_contents"));
+					rvo.setR_star(rs.getInt("r_star"));
+					rvo.setR_date(rs.getString("r_date"));
+					list.add(rvo);
+				}while(rs.next());
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace(); // e로 오류 받아서 오류 어디서 발생했는지 콘솔에 띄우기
+		}finally {
+			try {
+				if(conn!= null) 
+					conn.close();
+				if(ps != null) 
+					ps.close();
+				if(rs != null)
+					rs.close();
+			}catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+	//첫페이지에서 사용하는 최신리뷰 5개//
 	public static List<Review> getReviews(){
 		List<Review> list = null;
 		Connection conn = null;
@@ -89,9 +169,6 @@ public class ReviewDAO<Int> {
 		}
 		return list;
 	}
-		
-
-
 	//******* 리뷰 삭제 *******//
 	public static int delReview(int R_REVIEWID) {
 	
@@ -123,6 +200,7 @@ public class ReviewDAO<Int> {
 		}
 		return ret;
 	}
+	//
 }
 	
 	
